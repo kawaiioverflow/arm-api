58a3d5dd28bb3cb5e381c80218652408dc954ff5
P-Chan <hello@0x50.io>
2018-04-29 01:14:59 +0900

    feat: Add koa-hbs

---
+++ app/Controllers/HomeController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: async ctx => {
+    await ctx.render('home', {})
+  }
+}
--- start/app.js
+++ start/app.js
@@ -1,9 +1,19 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const hbs = require('koa-hbs')
 const logger = require('koa-logger')
+const path = require('path')
 const router = require('./routes')
 
 const app = new Koa()
+
+console.log(path.resolve(__dirname, '../app/Views'))
+
+app.use(
+  hbs.middleware({
+    viewPath: path.resolve(__dirname, '../app/Views')
+  })
+)
 
 app.use(logger())
 app.use(bodyParser())
